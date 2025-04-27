f59511989248f1a35ee2df5616b830c5df163fd2
Miguel Montalvo <miguel.montalvo@meetdomaine.com>
2025-04-26 20:22:17 -0600

    asdf

--- packages/vite-plugin-shopify/src/index.ts
+++ packages/vite-plugin-shopify/src/index.ts
@@ -10,6 +10,7 @@ import shopifyReactRefresh from './react-refresh'
 const vitePluginShopify = (options: Options = {}): Plugin[] => {
   const resolvedOptions = resolveOptions(options)
 
+  // ASDF
   const plugins = [
     // Apply plugin for configuring Vite settings
     shopifyConfig(resolvedOptions),
